02d16239f67d477707d7a6972bf4db91d65eb7a2
djtal <guillaume.garcera@gmail.com>
2010-12-02 21:01:43 +0100

    add css grid start playing with handlebars partial

--- js/scorecard/views.js
+++ js/scorecard/views.js
@@ -1,20 +1,44 @@
-var PlayerScoreLine = Backbone.View.extend({
+var PlayerScoreLineView = Backbone.View.extend({
 
-	className: "player",
-	 
-	initialize: function() {
-		_.bindAll(this, "render");
-		this.model.bind('change', this.render);
+    template: Handlebars.compile($("script[name=player]").html()),
+
+  
+  
+  className: "player",
+     
+    initialize: function() {
+        _.bindAll(this, "render");
+        this.model.bind('change', this.render);
   },
-	
+    
 })
 
 
 var PlayerNewView = Backbone.View.extend({
+  
+  template: Handlebars.compile($("script[name=playerNew]").html()),
 
+  render: function() {
+    $(this.el).html(this.template());
+    return this
+  },
+  
+})
 
-}
+var PlayersViews = Backbone.View.extend({
+  
+  template: Handlebars.compile($("script[name=players]").html()),
+  
+  partials: {
+    "player": $("script[name=player]").html(),
+  },
 
+  render: function() {
+    $(this.el).html(this.template(this.model.toJSON(), {"partials": this.partials}));
+    return this
+  },
+  
+})
 
 
 var BoardGameScoreCard = Backbone.View.extend({
@@ -23,7 +47,7 @@ var BoardGameScoreCard = Backbone.View.extend({
   
   className: "boardgame",
   
-  template: Handlebars.compile("<h1>{{name}}</h1><div>hey this is a simple for the moment.<br /> Just learn the whole thing</div>"),
+  template: Handlebars.compile($("script[name=appView]").html()),
 
   events: {
     "click .icon":          "open",
@@ -35,13 +59,14 @@ var BoardGameScoreCard = Backbone.View.extend({
     _.bindAll(this, "render");
     this.model.bind('change', this.render)
     this.newPlayerView = new PlayerNewView()
+    this.playersView = new PlayersViews({model: this.model})
   },
 
   render: function() {
     $(this.el).html(this.template(this.model.toJSON()));
     $(this.el).append(this.newPlayerView.render().el)
+    $(this.el).append(this.playersView.render().el)
+
+    return this
   }
-  
-
-
 });
